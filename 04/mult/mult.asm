69c09990817f0ee4e668e8f0db7fd7e0480dcff6
egeerdogan <ege@egeerdogan.com>
2019-01-20 10:16:27 +0300

    project 4 complete

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -6,4 +6,37 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// Since there is no direct multiplication operation,
+// we have to perform a series of additions.
+
+@R0
+D = M;
+
+@i						// i (the letter) will be our counter, loop will end when i = 0
+M = D;
+
+@R2
+M = 0;				// set mult. to 0 in case it was non-zero before
+
+(LOOP)				// loop begins here
+	@i
+	D = M;	  	// retrieve i
+
+	@END
+	D; JEQ			// End loop if i = 0
+
+	@R1
+	D = M;			// retrieve the number that we add to itself
+
+	@R2
+	M = M + D;	// the operation that we are repeating	
+
+	@i
+	M = M - 1;  // decrement i			
+
+	@LOOP
+	0; JMP			// Goto LOOP
+
+(END)
+	@END
+	0; JMP			// infinite loop to stop the loop
